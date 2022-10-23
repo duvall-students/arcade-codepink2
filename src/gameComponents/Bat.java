f8e69dd42584571c66af8eafc97c7bc4ea3ee31d
shannonseignious <shannonseignious@macbook-air-2.elon.edu>
2022-10-23 18:23:44 -0400

    Created player device super class and bat/shooter subclasses

--- src/gameComponents/Bat.java
+++ src/gameComponents/Bat.java
@@ -1,5 +1,33 @@
 package gameComponents;
 
-public class Bat {
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+public class Bat extends PlayerDevice {
+
+	//initialize fields
+	int playerDeviceWidth = 150;
+	int playerDeviceHeight = 30;
+
+	private ImageView playerBat;
+
+	public Bat(Image batImage) {
+		// Displays ImageView the image as is
+		playerBat = new ImageView();
+		playerBat.setImage(batImage);
+		// Scales image so that it's smaller
+		playerBat.setFitWidth(playerDeviceWidth);
+		playerBat.setFitHeight(playerDeviceHeight);
+		playerBat.setPreserveRatio(true);
+		playerBat.setSmooth(true);
+		playerBat.setCache(true);
+		playerDevice = playerBat;
+
+	}
+	
+	@Override
+	public Node getView() {
+		return playerBat;
+	}
 }
--- src/gamePlay/Game.java
+++ src/gamePlay/Game.java
@@ -1,5 +1,29 @@
 package gamePlay;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Rectangle;
+import gameComponents.PlayerDevice;
+
 public class Game {
+	
+	public static final int MOVER_SPEED = 15;
+	
+	 public void handleKeyInput (KeyCode code, PlayerDevice playerDevice) {
+	    	
+	        if (code == KeyCode.LEFT) {
+	        	playerDevice.setX(playerDevice.getX() - MOVER_SPEED);
+	            if (playerDevice.getX() <= 0) {
+	            	playerDevice.setX(0);
+	            }
+	        }
+	        else if (code == KeyCode.RIGHT) {
+	        	playerDevice.setX(playerDevice.getX() + MOVER_SPEED);
+	            if (playerDevice.getX() >= 235) {
+	            	playerDevice.setX(235);
+	            }
+	        }
+	    }
+	 
+	 public boolean hasWon(int breakerCount);
 
 }
