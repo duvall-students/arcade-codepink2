cfdb1551606d14662594072bd5f935aad42d3e38
shannonseignious <shannonseignious@macbook-air-2.elon.edu>
2022-10-31 15:24:02 -0400

    Updated breakout and galaga subclasses

--- src/gamePlay/Breakout.java
+++ src/gamePlay/Breakout.java
@@ -2,31 +2,31 @@ package gamePlay;
 
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
 import gameComponents.Ball;
 
 //@author: Shannon Seignious
 public class Breakout extends Game {
-
-	Rectangle brick;
 	
 	public Paint BRICKCOLOR = Color.web("#db027e");
 	public static final Paint BACKGROUND = Color.web("#feb4de");
 	
-	public static final int BALLXDIRECTION = -2;
-	public static final int BALLYDIRECTION = -2;
+	protected static final int BALLXDIRECTION = -2;
+	protected static final int BALLYDIRECTION = -2;
+	protected static final int MAXBRICKCOUNT = 84;
+	protected static final int STARTING_X_POSITION = 192;
+	protected static final int STARTING_Y_POSITION = 250;
     
+	
     @Override
     public boolean hasWon(int brickcount) {
-    	if (brickcount == 84) {
+    	if (brickcount == MAXBRICKCOUNT) {
     		return true;
     	}
     	return false;
     }
     
     public void resetGame(Ball ball) {
-    	ball.setPosition(192, 250);
+    	ball.setPosition(STARTING_X_POSITION, STARTING_Y_POSITION);
     }
-    
 
 }
--- src/gamePlay/Galaga.java
+++ src/gamePlay/Galaga.java
@@ -1,5 +1,16 @@
 package gamePlay;
 
+//@author: Shannon Seignious
 public class Galaga extends Game {
+	
+	protected static final int MAX_BADGUY_COUNT = 40;
+	
+	@Override
+	public boolean hasWon(int badGuyCount) {
+		if (badGuyCount == MAX_BADGUY_COUNT) {
+    		return true;
+    	}
+    	return false;
+	}
 
 }
