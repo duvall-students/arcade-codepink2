fc502710564565732029d5237680c3ba462fb9ff
shannonseignious <shannonseignious@macbook-air-2.elon.edu>
2022-11-01 23:16:28 -0400

    Cleaned up code - added comments, no duplicated code

--- src/gameComponents/PlayerDevice.java
+++ src/gameComponents/PlayerDevice.java
@@ -1,15 +1,32 @@
 package gameComponents;
 
 import javafx.scene.Node;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+//@author: Shannon Seignious
 public abstract class PlayerDevice {
 	
+	//initialize fields
 	public int playerDeviceWidth;
 	public int playerDeviceHeight;
 	
 	protected ImageView playerDevice;
 	
+	// Player Device constructor
+	public PlayerDevice(Image playerDeviceImage) {
+		// Displays ImageView the image as is
+		playerDevice = new ImageView();
+		playerDevice.setImage(playerDeviceImage);
+		// Scales image so that it's smaller
+		playerDevice.setFitWidth(playerDeviceWidth);
+		playerDevice.setFitHeight(playerDeviceHeight);
+		playerDevice.setPreserveRatio(true);
+		playerDevice.setSmooth(true);
+		playerDevice.setCache(true);
+	}
+	
+	// initializes x position of device
 	public void setXPosition(double X) {
 		playerDevice.setX(X);
 	}
--- src/gameComponents/Shooter.java
+++ src/gameComponents/Shooter.java
@@ -1,28 +1,21 @@
 package gameComponents;
 
 import javafx.scene.Node;
-import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 //@author: Shannon Seignious
 public class Shooter extends PlayerDevice {
 
+	//initialize fields
 	int playerDeviceWidth = 50;
 	int playerDeviceHeight = 50;
 
 	private ImageView playerShooter;
 	
+	// Shooter Constructor
 	public Shooter(Image shooterImage) {
-		// Displays ImageView the image as is
-		playerShooter = new ImageView();
-		playerShooter.setImage(shooterImage);
-		// Scales image so that it's smaller
-		playerShooter.setFitWidth(playerDeviceWidth);
-		playerShooter.setFitHeight(playerDeviceHeight);
-		playerShooter.setPreserveRatio(true);
-		playerShooter.setSmooth(true);
-		playerShooter.setCache(true);
+		super(shooterImage);
 		playerDevice = playerShooter;
 	}
 	
