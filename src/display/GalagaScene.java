dcb104f7c5ec781f92d1b3247b1f441feffe6af1
Milli K <85273731+akearse444@users.noreply.github.com>
2022-11-07 19:19:47 -0500

    Update GalagaScene.java

--- src/display/GalagaScene.java
+++ src/display/GalagaScene.java
@@ -33,7 +33,9 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 //@author: Milli Kearse
-public class GalagaScene extends SetScene{
+//This class shows the scene for the Galaga game.
+public class GalagaScene extends SetScene {
+
 	public static final String TITLE = "Galaga";
 	private Scene myScene;
 	private Scene levelOneScene;
@@ -50,24 +52,27 @@ public class GalagaScene extends SetScene{
 	public static final int FRAMES_PER_SECOND = 60;
 	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
 	public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-	
+
 	public static final String GREEN_BAD_GUY_IMAGE = "resources/GreenBadGuy.gif";
 	public static final String RED_BAD_GUY_IMAGE = "resources/RedBadGuy.gif";
 	public static final String BLUE_BAD_GUY_IMAGE = "resources/BlueBadGuy.gif";
-	public static final String MY_SHOOTER = "resources/galaga_shooter.png";
+	public static final String 	MY_PLAYER = "resources/galaga_shooter.png";
 
 	File file = new File("TOPSCORE");
+
 	private List<GreenBadGuy> myGreenBadGuys;
 	private List<RedBadGuy> myRedBadGuys;
 	private List<BlueBadGuy> myBlueBadGuys;
 
-	//Game gamer = new Game();
 	Galaga gamer = new Galaga();
+	//Game gamer = new Game();
+
 	GreenBadGuy green;
 	RedBadGuy red;
 	BlueBadGuy blue;
+	PlayerDevice device;
 	Player player;
-	PlayerDevice shooter;
+	Shooter shooter;
 	Timeline animation;
 
 	Text displayScore = new Text();
@@ -82,7 +87,7 @@ public class GalagaScene extends SetScene{
 	@Override
 	public void start (Stage stage) {
 		// attach scene to the stage and display it
-		myScene = setUp(width, height, BACKGROUND);
+		myScene = setUpGame(width, height, BACKGROUND);
 		stage.setScene(myScene);
 		stage.setTitle(TITLE);
 		stage.show();
@@ -90,112 +95,91 @@ public class GalagaScene extends SetScene{
 		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY, stage));;
 		animation = new Timeline();
 		animation.setCycleCount(Timeline.INDEFINITE);
-		animation.getKeyFrames().add(frame);
+		//animation.getKeyFrames().add(frame);
 		animation.play();
 	}
 
-	public Scene setUp (int width, int height, Paint background) {
+	public Scene setUpGame (int width, int height, Paint background) {
 		Group root = new Group();
 		player = new Player();
-		
-		//add the shooter
-		try {
-			Image imageShooter = new Image(new FileInputStream(MY_SHOOTER));
-			shooter = new Shooter(imageShooter);
-			root.getChildren().add(shooter.getView());
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
 
 		int BRICKXPOSITION = 0;
 		int BRICKYPOSITION = 0;
 
-		myGreenBadGuys = new ArrayList<>(); 
-		myRedBadGuys = new ArrayList<>();
+		myGreenBadGuys = new ArrayList<>();  
+		myRedBadGuys = new ArrayList<>();  
 		myBlueBadGuys = new ArrayList<>();
-		
+
 		while (BRICKYPOSITION <= height/2.5) {
 			while (BRICKXPOSITION <= width) {
 				try {
 					Image imageGreen = new Image(new FileInputStream(GREEN_BAD_GUY_IMAGE));
 					green = new GreenBadGuy(imageGreen, BRICKXPOSITION, BRICKYPOSITION);
-					root.getChildren().add(green.getView());
-					
+
 					Image imageRed = new Image(new FileInputStream(RED_BAD_GUY_IMAGE));
 					red = new RedBadGuy(imageRed, BRICKXPOSITION, BRICKYPOSITION);
-					root.getChildren().add(red.getView());
-					
+
 					Image imageBlue = new Image(new FileInputStream(BLUE_BAD_GUY_IMAGE));
 					blue = new BlueBadGuy(imageBlue, BRICKXPOSITION, BRICKYPOSITION);
-					root.getChildren().add(blue.getView());
 				}
 				catch (FileNotFoundException e) {
 					e.printStackTrace();
 				}
-				
 				myGreenBadGuys.add(green);
 				myRedBadGuys.add(red);
 				myBlueBadGuys.add(blue);
+				root.getChildren().add(green.getView()); 
+				root.getChildren().add(red.getView()); 
+				root.getChildren().add(blue.getView()); 
+
 				BRICKXPOSITION += green.getWidth() + 5;   
 			}
 			this.green.setPoints(green.getPoints() - 1);
 			BRICKYPOSITION += green.getHeight() + 5; 
 			BRICKXPOSITION = 0;
 		}
-		
-		//calls the SetScene class to load all text display
+
+		//calls the SetScene class to load all text display.
 		super.createGroup();
-		super.createGroup().getChildren().addAll(displayScore, topScore, level);
-		
 		Scene scene = new Scene(root, width, height, background);
 		//respond to input 
-		scene.setOnKeyPressed(e -> gamer.handleKeyInput(e.getCode(), shooter));
+		scene.setOnKeyPressed(e -> gamer.handleKeyInput(e.getCode(), device));
 		return scene;
 	}
 
 	private void step (double elapsedTime, Stage stage) {
 		int count = 0;
 
-		ball.moveBall(elapsedTime);
-		for (Bricks brick : myBricks) {
-			if (brick.getView().getBoundsInParent().intersects(ball.getView().getBoundsInParent()) && brick.visible()) {
+		for (GreenBadGuy green : myGreenBadGuys) {
+			for (RedBadGuy red: myRedBadGuys) {
+				for (BlueBadGuy blue: myBlueBadGuys) {
+					player.setScore(player.getScore());
+					displayScore.setText("SCORE: " + String.valueOf(player.getScore()));
+					System.out.println(player.getScore());
 
-				player.setScore(brick.getPoints() + player.getScore());
-				displayScore.setText("SCORE: " + String.valueOf(player.getScore()));
-
-				//once all bricks are cleared, go to level one
-				if(player.getScore() == 477) {
-					count += 1;
-					try {
-						levelOneScene = levelOne(width, height, LEVEL_ONE_BACKGROUND);
+					//once all bad guys are cleared, go to level one
+					if(player.getScore() == 477) {
+						count += 1;
+						levelOneScene = setUpGame(width, height, LEVEL_ONE_BACKGROUND);
 						stage.setScene(levelOneScene);
-	
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
+					}
+
+					//once all bad guys are cleared in level 1, go to level 2
+					if(player.getScore() == 954) {
+						count += 1;
+						levelTwoScene = setUpGame(width, height, LEVEL_TWO_BACKGROUND);
+						stage.setScene(levelTwoScene);
+					}
+
+					//once all bad guys are cleared in level 2, go to level 3
+					if(player.getScore() == 1431) {
+						count += 1;
+						levelThreeScene = setUpGame(width, height, LEVEL_THREE_BACKGROUND);
+						stage.setScene(levelThreeScene);
 					}
 				}
-
-				//once all bricks are cleared in level 1, go to level 2
-				if(player.getScore() == 954) {
-					count += 1;
-					levelTwoScene = levelTwo(width, height, LEVEL_TWO_BACKGROUND);
-					stage.setScene(levelTwoScene);
-				}
-
-				//once all bricks are cleared in level 2, go to level 3
-				if(player.getScore() == 1431) {
-					count += 1;
-					levelThreeScene = levelThree(width, height, LEVEL_THREE_BACKGROUND);
-					stage.setScene(levelThreeScene);
-				}
 			}
-		}
 
-		if (ball.dropsOff(myScene.getWidth(), myScene.getHeight())){
-			player.lifeLost();
-			displayLives.setText("LIVES: " + String.valueOf(player.getLives()));
-			gamer.resetGame(ball);
 		}
 
 		//all levels cleared, gamer.hasWon
@@ -250,169 +234,4 @@ public class GalagaScene extends SetScene{
 			ex.printStackTrace();
 		}
 	}
-
-	/*public Scene levelOne(int width, int height, Paint background) {
-		Group root = new Group();
-		player = new Player();
-		try {
-			Image imageBall = new Image(new FileInputStream(BOUNCER_IMAGE));
-			ball = new Ball(imageBall);
-			root.getChildren().add(ball.getView());
-		}
-		catch (FileNotFoundException e) {}
-		bat = new Bat().createBat();
-
-		int BRICKXPOSITION = 0;
-		int BRICKYPOSITION = 0;
-
-		myBricks = new ArrayList<>();   
-		while (BRICKYPOSITION <= height/2.5) {
-			while (BRICKXPOSITION <= width) {
-				try {
-					Image imageBrick = new Image(new FileInputStream(BRICK_IMAGE));
-					brick = new Bricks(imageBrick, BRICKXPOSITION, BRICKYPOSITION);
-				}
-				catch (FileNotFoundException e) {}
-				myBricks.add(brick);
-				root.getChildren().add(brick.getView()); 
-				BRICKXPOSITION += brick.getWidth() + 5;   
-			}
-			this.brick.setPoints(brick.getPoints() - 1);
-			BRICKYPOSITION += brick.getHeight() + 5; 
-			BRICKXPOSITION = 0;
-		}
-
-
-		displayScore.setText("SCORE:" + String.valueOf(player.SCORE));
-		displayScore.setFill(Color.WHITE);
-		displayScore.setFont(Font.font(18));
-		displayScore.setX(10);
-		displayScore.setY(482);
-
-		displayLives.setText("LIVES:" + String.valueOf(player.getLives()));
-		displayLives.setFill(Color.WHITE);
-		displayLives.setFont(Font.font(18));
-		displayLives.setX(10);
-		displayLives.setY(465);
-
-		root.getChildren().add(bat);
-		root.getChildren().addAll(displayScore);
-		root.getChildren().add(displayLives);
-		root.getChildren().add(lostMessage);
-		root.getChildren().add(winMessage);
-		Scene scene1 = new Scene(root, width, height, background);
-		//respond to input 
-		scene1.setOnKeyPressed(e -> new Game().handleKeyInput(e.getCode(), bat));
-		return scene1;
-	}
-
-	public Scene levelTwo(int width, int height, Paint background) {
-		Group root = new Group();
-		player = new Player();
-		try {
-			Image imageBall = new Image(new FileInputStream(BOUNCER_IMAGE));
-			ball = new Ball(imageBall);
-			root.getChildren().add(ball.getView());
-		}
-		catch (FileNotFoundException e) {}
-		bat = new Bat().createBat();
-
-		int BRICKXPOSITION = 0;
-		int BRICKYPOSITION = 0;
-
-		myBricks = new ArrayList<>();   
-		while (BRICKYPOSITION <= height/2.5) {
-			while (BRICKXPOSITION <= width) {
-				try {
-					Image imageBrick = new Image(new FileInputStream(BRICK_IMAGE));
-					brick = new Bricks(imageBrick, BRICKXPOSITION, BRICKYPOSITION);
-				}
-				catch (FileNotFoundException e) {}
-				myBricks.add(brick);
-				root.getChildren().add(brick.getView()); 
-				BRICKXPOSITION += brick.getWidth() + 5;   
-			}
-			this.brick.setPoints(brick.getPoints() - 1);
-			BRICKYPOSITION += brick.getHeight() + 5; 
-			BRICKXPOSITION = 0;
-		}
-
-
-		displayScore.setText("SCORE:" + String.valueOf(player.SCORE));
-		displayScore.setFill(Color.WHITE);
-		displayScore.setFont(Font.font(18));
-		displayScore.setX(10);
-		displayScore.setY(482);
-
-		displayLives.setText("LIVES:" + String.valueOf(player.getLives()));
-		displayLives.setFill(Color.WHITE);
-		displayLives.setFont(Font.font(18));
-		displayLives.setX(10);
-		displayLives.setY(465);
-
-		root.getChildren().add(bat);
-		root.getChildren().addAll(displayScore);
-		root.getChildren().add(displayLives);
-		root.getChildren().add(lostMessage);
-		root.getChildren().add(winMessage);
-		Scene scene1 = new Scene(root, width, height, background);
-		//respond to input 
-		scene1.setOnKeyPressed(e -> new Game().handleKeyInput(e.getCode(), bat));
-		return scene1;
-	}
-
-	public Scene levelThree(int width, int height, Paint background) {
-		Group root = new Group();
-		player = new Player();
-		try {
-			Image imageBall = new Image(new FileInputStream(BOUNCER_IMAGE));
-			ball = new Ball(imageBall);
-			root.getChildren().add(ball.getView());
-		}
-		catch (FileNotFoundException e) {}
-		bat = new Bat().createBat();
-
-		int BRICKXPOSITION = 0;
-		int BRICKYPOSITION = 0;
-
-		myBricks = new ArrayList<>();   
-		while (BRICKYPOSITION <= height/2.5) {
-			while (BRICKXPOSITION <= width) {
-				try {
-					Image imageBrick = new Image(new FileInputStream(BRICK_IMAGE));
-					brick = new Bricks(imageBrick, BRICKXPOSITION, BRICKYPOSITION);
-				}
-				catch (FileNotFoundException e) {}
-				myBricks.add(brick);
-				root.getChildren().add(brick.getView()); 
-				BRICKXPOSITION += brick.getWidth() + 5;   
-			}
-			this.brick.setPoints(brick.getPoints() - 1);
-			BRICKYPOSITION += brick.getHeight() + 5; 
-			BRICKXPOSITION = 0;
-		}
-
-
-		displayScore.setText("SCORE:" + String.valueOf(player.SCORE));
-		displayScore.setFill(Color.WHITE);
-		displayScore.setFont(Font.font(18));
-		displayScore.setX(10);
-		displayScore.setY(482);
-
-		displayLives.setText("LIVES:" + String.valueOf(player.getLives()));
-		displayLives.setFill(Color.WHITE);
-		displayLives.setFont(Font.font(18));
-		displayLives.setX(10);
-		displayLives.setY(465);
-
-		root.getChildren().add(bat);
-		root.getChildren().addAll(displayScore);
-		root.getChildren().add(displayLives);
-		root.getChildren().add(lostMessage);
-		root.getChildren().add(winMessage);
-		Scene scene1 = new Scene(root, width, height, background);
-		//respond to input 
-		scene1.setOnKeyPressed(e -> new Game().handleKeyInput(e.getCode(), bat));
-		return scene1;
-	}*/
 }
